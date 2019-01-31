7e2cafa41e587fcb2e4d1426e70b1f0cce9103a6
Janessa Tran <tranjanessa@gmail.com>
2019-01-30 22:08:35 -0600

    save changes

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,26 @@
+var validNumbers = ['0','1','2','3','4','5','6','7','8','9'];
+var validOperators = ['-', '*', '+', '=', '/', '%', 'Backspace'];
+
+document.onkeypress = function(evt) {
+    evt = evt || window.event;
+	var charCode = evt.key || evt.which;
+	var display = document.getElementById('calc-screen')
+	console.log(charCode);
+	if (!validNumbers.includes(charCode) && !validOperators.includes(charCode)){
+		alert("Please enter numeric values or operators only.")
+	}
+	// update display
+	else {
+		display.textContent = (display.textContent) + charCode;
+
+		// if (display.textContent.length == 0) {
+		// 	display.textContent = charCode;
+		// }
+		// else {
+		// }
+	}
+};
+
 function add (a, b) {
 	return a + b;
 }
